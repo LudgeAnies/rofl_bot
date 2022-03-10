b04624e404b1ece68a96aca9dd22dd8afb7077fe
LudgeAnies <maks.xaskov12@gmail.com>
2022-03-10 18:47:06 +0300

    небольшие доработки

--- main.py
+++ main.py
@@ -4,21 +4,21 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 import logging
 from time import sleep
+
 bot = Bot(token='5249454736:AAGR3O6RcXnH3Xcy1zBh8sraQUmFeCtfXYQ')
 dp = Dispatcher(bot)
 logging.basicConfig(level=logging.INFO)
 
 @dp.message_handler(commands=['start'])
-async def start_command(message: types.Message):
-    await message.reply('Привет!\nМы сообщим, как только цена на товар поменяется:)')
+async def start_command(msg: types.Message):
+    await msg.reply('Привет!\nМы сообщим, как только цена на товар поменяется:)')
     old_price = new_price()[1]
     while True:
         if old_price != new_price()[1]:
-            await message.reply(f'Новая цена: {new_price()[1]}\n Старая цена: {old_price}\n Цена изменилась на {old_price - new_price()[1]}')
+            await msg.reply(f'Новая цена: {new_price()[1]}\n Старая цена: {old_price}\n Цена изменилась на {old_price - new_price()[1]}')
         else:
             continue
         sleep(3)
-
 
 async def new_price():
     chrome_options = Options()
@@ -28,5 +28,6 @@ async def new_price():
     price = int(driver.find_element(By.TAG_NAME, 'h2')).text
     return text, price
 
-bot.polling(none_stop=True, interval=0)
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
 
