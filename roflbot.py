07ca48d6e1b56ef815a681e3707ac1b01bec60f1
LudgeAnies <maks.xaskov12@gmail.com>
2022-03-11 16:38:18 +0300

    доработал, полный функционал

--- roflbot.py
+++ roflbot.py
@@ -14,18 +14,20 @@ logging.basicConfig(level=logging.INFO)
 
 @dp.message_handler(commands=['start'])
 async def start_command(msg: types.Message):
-    await msg.reply('Привет!\nМы сообщим, как только цена на товар поменяется:)')
+    await msg.reply('Привет!'
+                    '\nЦена на товар может периодически меняться'
+                    '\nОбо всех изменениях мы будем сообщать тебе:)')
     date: datetime.datetime = datetime.datetime.now()
 
     resp: requests.Response = requests.get(url)
     soup: bs4.BeautifulSoup = BeautifulSoup(resp.content, 'html.parser')
 
-    old_price: int = get_price(soup)
+    old_price: str = get_price(soup)
     date_format: str = date.strftime("%d/%m/%Y  %H:%M:%S")
     while True:
         resp: requests.Response = requests.get(url)
-        soup: bs4.BeautifulSoup = BeautifulSoup(resp.content, 'html5lib')
-        actual_price: int = get_price(soup)
+        soup: bs4.BeautifulSoup = BeautifulSoup(resp.content, 'html.parser') #html5lib
+        actual_price: str = get_price(soup)
         if old_price != actual_price:
             await msg.reply(f'Название товара: {get_text_price(soup)}'
                             f'\nНовая цена: {actual_price}'
@@ -34,8 +36,8 @@ async def start_command(msg: types.Message):
             old_price = actual_price
 
 
-def get_price(soup: bs4.BeautifulSoup) -> int:
-    return int(soup.find('h2').text.strip())
+def get_price(soup: bs4.BeautifulSoup) -> str:
+    return str(soup.find('h2').text.strip())
 
 
 def get_text_price(soup: bs4.BeautifulSoup) -> str:
