ef05479d15269d40482414cf63ea7d532b8fe516
LudgeAnies <maks.xaskov12@gmail.com>
2022-03-17 19:03:19 +0300

    исправления

--- roflbot.py
+++ roflbot.py
@@ -5,7 +5,6 @@ import bs4
 import requests
 from aiogram import *
 from bs4 import BeautifulSoup
-from time import sleep
 
 url = 'http://gendalf.cf/'
 bot = Bot(token='5249454736:AAGR3O6RcXnH3Xcy1zBh8sraQUmFeCtfXYQ')
@@ -18,28 +17,34 @@ async def start_command(msg: types.Message):
     await msg.reply('Привет!'
                     '\nЦена на товар может периодически меняться'
                     '\nОбо всех изменениях мы будем сообщать тебе:)')
+
+@dp.message_handler(commands=['check'])
+async def check_command(msg: types.Message):
     date: datetime.datetime = datetime.datetime.now()
 
     resp: requests.Response = requests.get(url)
     soup: bs4.BeautifulSoup = BeautifulSoup(resp.content, 'html.parser')
 
-    old_price: str = get_price(soup)
+    old_price: int = get_price(soup)
     date_format: str = date.strftime("%d/%m/%Y  %H:%M:%S")
     while True:
         resp: requests.Response = requests.get(url)
-        soup: bs4.BeautifulSoup = BeautifulSoup(resp.content, 'html.parser') #html5lib
-        actual_price: str = get_price(soup)
+        soup: bs4.BeautifulSoup = BeautifulSoup(resp.content, 'html.parser')
+        actual_price: int = get_price(soup)
         if old_price != actual_price:
-            await msg.reply(f'Название товара: {get_text_price(soup)}'
-                            f'\nНовая цена: {actual_price}'
-                            f'\nСтарая цена: {old_price}'
-                            f'\nДата: {date_format}')
+            difference: int = old_price - actual_price
+            await msg.reply(f'Название товара:{get_text_price(soup)}'
+                            f'\nНовая цена: {actual_price}₽'
+                            f'\nСтарая цена: {old_price}₽'
+                            f'\nДата изменения: {date_format}'
+                            f'\nРазница в цене: {difference}₽')
             old_price = actual_price
-        sleep(10)
 
 
-def get_price(soup: bs4.BeautifulSoup) -> str:
-    return str(soup.find('h2').text.strip())
+def get_price(soup: bs4.BeautifulSoup) -> int:
+    x = soup.find('h2').text.strip()
+    x = x.replace('Цена:', '').replace('₽','').replace(' ', '')
+    return int(x)
 
 
 def get_text_price(soup: bs4.BeautifulSoup) -> str:
--- roflbot2.py
+++ roflbot2.py
@@ -1,11 +1,10 @@
 import datetime
 import logging
 
-import bs4
 import requests
-from aiogram import *
+import bs4
 from bs4 import BeautifulSoup
-from time import sleep
+from aiogram import *
 
 url = 'https://goo.su/EseK/'
 bot = Bot(token='5249454736:AAGR3O6RcXnH3Xcy1zBh8sraQUmFeCtfXYQ')
@@ -23,23 +22,26 @@ async def start_command(msg: types.Message):
     resp: requests.Response = requests.get(url)
     soup: bs4.BeautifulSoup = BeautifulSoup(resp.content, 'html.parser')
 
-    old_price: str = get_price(soup)
+    old_price: int = get_price(soup)
     date_format: str = date.strftime("%d/%m/%Y  %H:%M:%S")
     while True:
         resp: requests.Response = requests.get(url)
-        soup: bs4.BeautifulSoup = BeautifulSoup(resp.content, 'html.parser') #html5lib
-        actual_price: str = get_price(soup)
+        soup: bs4.BeautifulSoup = BeautifulSoup(resp.content, 'html.parser')
+        actual_price: int = get_price(soup)
         if old_price != actual_price:
+            difference: int = old_price - actual_price
             await msg.reply(f'Название товара: {get_text_price(soup)}'
-                            f'\nНовая цена: {actual_price}'
-                            f'\nСтарая цена: {old_price}'
-                            f'\nДата: {date_format}')
+                            f'\nНовая цена: {actual_price}₽'
+                            f'\nСтарая цена: {old_price}₽'
+                            f'\nДата: {date_format}'
+                            f'\nРазница в цене: {difference}₽')
             old_price = actual_price
-        sleep(10)
 
 
-def get_price(soup: bs4.BeautifulSoup) -> str:
-    return str(soup.find('span', class_='woocommerce-Price-amount amount').text.strip())
+def get_price(soup: bs4.BeautifulSoup) -> int:
+    x = soup.find('span', class_='woocommerce-Price-amount').text.split('.')[0]
+    x = x.replace('₽', '')
+    return int(x)
 
 
 def get_text_price(soup: bs4.BeautifulSoup) -> str:
